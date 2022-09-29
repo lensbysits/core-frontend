ca4c0db9bae61a1756cac7e09915a2fc233107de
Bart van der Vliet <bartv@od.org>
2022-09-29 10:55:15 +0200

    Refactored user context service to not expose subjects but observables. Added UserContextService documentation.

--- core/app/abstract/src/lib/modules/user-context/services/default-user-context.service.ts
+++ core/app/abstract/src/lib/modules/user-context/services/default-user-context.service.ts
@@ -9,22 +9,26 @@ export const DEFAULT_USER = new InjectionToken<{UserData: UserData, IsAuthentica
 export class DefaultUserContextService extends UserContextService implements OnDestroy {
   protected readonly destroy$ = new Subject<void>();
 
-  readonly Changed$: Subject<void> = new Subject();
-  readonly UserData$: BehaviorSubject<UserData> = new BehaviorSubject(this.UserData);
-  readonly IsAuthenticated$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private readonly changedSubject: Subject<void> = new Subject();
+  private readonly userDataSubject: BehaviorSubject<UserData> = new BehaviorSubject(this.UserData);
+  private readonly isAuthenticatedSubject: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  public changed$ = this.changedSubject.asObservable();
+  public userData$ = this.userDataSubject.asObservable();
+  public isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
 
   public get UserData(): UserData {
-    return this.UserData$?.value;
+    return this.userDataSubject?.value;
   }
   private set UserData(userData: UserData) {
-    this.UserData$.next(userData);
+    this.userDataSubject.next(userData);
   }
 
   public get IsAuthenticated(): boolean {
-    return this.IsAuthenticated$.value;
+    return this.isAuthenticatedSubject.value;
   }
   private set IsAuthenticated(isAuthenticated: boolean) {
-    this.IsAuthenticated$.next(isAuthenticated);
+    this.isAuthenticatedSubject.next(isAuthenticated);
   }
 
   constructor(@Optional() @Inject(DEFAULT_USER) defaultUser?: {UserData: UserData, IsAuthenticated: boolean}) {
@@ -35,13 +39,13 @@ export class DefaultUserContextService extends UserContextService implements OnD
   }
 
   IsInRole$(role: string): Observable<boolean> {    
-    return this.UserData$.pipe(map(userData => userData?.Roles?.includes(role) ?? false));
+    return this.userDataSubject.pipe(map(userData => userData?.Roles?.includes(role) ?? false));
   };
 
   IsInRole = (role: string) => this.UserData.Roles?.includes(role) ?? false;
   
   HasClaim$(claim: string): Observable<boolean> {
-    return this.UserData$.pipe(map(userData => (userData?.Claims?.findIndex(c => c.name === claim) ?? -1) > -1));
+    return this.userDataSubject.pipe(map(userData => (userData?.Claims?.findIndex(c => c.name === claim) ?? -1) > -1));
   };
   
   HasClaim = (claim: string) => (this.UserData.Claims?.findIndex(c => c.name === claim) ?? -1) > -1
@@ -49,7 +53,7 @@ export class DefaultUserContextService extends UserContextService implements OnD
   protected Set(userData: UserData, isAuthenticated: boolean): void {
     this.IsAuthenticated = isAuthenticated;
     this.UserData = { ...this.UserData, ...userData };
-    this.Changed$.next();
+    this.changedSubject.next();
   }
 
   ngOnDestroy(): void {
