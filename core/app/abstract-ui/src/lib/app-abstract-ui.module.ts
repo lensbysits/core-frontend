e39350e404223dab6883a7db328e497dfeffc61b
jstorteboom <johan@storteboom.net>
2022-10-10 11:16:56 +0200

    Add comment as to why APP_INITIALIZER is in app-abstract-ui-module.ts.

--- core/app/abstract-ui/src/lib/app-abstract-ui.module.ts
+++ core/app/abstract-ui/src/lib/app-abstract-ui.module.ts
@@ -14,6 +14,12 @@ const components_directives = [AppComponent, ComponentLoaderDirective];
   exports: [...components_directives, RouterModule],
   providers: [
     { provide: DialogService, useClass: DefaultDialogService },
+    /**** 
+      JST - 10-10-2022
+      This APP_INITIALZER serves the purpose of instantiating an ErrorHandlerService at startup.
+      Without this, the ErrorHandlerService may never be instantiated.
+      This is subject to change as we learn more of the different scenario's errorhandling is used in.
+    ****/
     {
       provide: APP_INITIALIZER,
       multi: true,
