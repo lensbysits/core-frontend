6afd1b3f9e569c75b45bf42e61ce53c487f9737e
Bart van der Vliet <bartv@od.org>
2022-11-28 16:28:16 +0100

    Fix autocomplete bug when selecting an item and then changing the search term.

--- core/ui/prime/components/src/lib/autocomplete/autocomplete.component.ts
+++ core/ui/prime/components/src/lib/autocomplete/autocomplete.component.ts
@@ -25,5 +25,12 @@ export class AutoCompleteComponent extends InputBaseComponent implements OnInit 
 
     public onSearched(event: any): void {
         this.filteredOptions = this.options?.filter(option => option.value.toLowerCase().indexOf(event.query.toLowerCase()) >= 0);
+        this.value = undefined;
+        this.valueChanged();
+    }
+
+    public onCleared(event: any): void {
+        this.value = undefined;
+        this.valueChanged();
     }
 }
