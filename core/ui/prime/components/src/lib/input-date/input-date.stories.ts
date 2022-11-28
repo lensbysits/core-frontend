46a7656bfc5f3ed103d519e2c5adae67bfaee6c6
Bart van der Vliet <bartv@od.org>
2022-11-28 13:55:27 +0100

    Fixed stories.

--- core/ui/prime/components/src/lib/input-date/input-date.stories.ts
+++ core/ui/prime/components/src/lib/input-date/input-date.stories.ts
@@ -1,21 +1,36 @@
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { Meta, moduleMetadata, Story } from "@storybook/angular";
 import { InputDateComponent } from "./input-date.component";
-import { CalendarModule } from "primeng/calendar";
+import { InputDateModule } from "./input-date.module";
+
+const form: FormGroup = new FormGroup({
+    date: new FormControl("", []),
+});
 
 export default {
     component: InputDateComponent,
     title: "Components/InputDate",
     decorators: [
         moduleMetadata({
-            imports: [ CalendarModule, BrowserAnimationsModule ]
+            imports: [ InputDateModule, BrowserAnimationsModule, ReactiveFormsModule ]
         })
     ]
 } as Meta
 
 const Template: Story = args => ({
+    template:  `<form [formGroup]="form">
+                <lens-input-date
+                    formControlName="date"
+                    name="date"
+                    [id]="id"
+                    [mode]="mode"
+                    [placeholder]="placeholder">
+                </lens-input-date>
+                </form>`,
     props: {
-        ...args
+        ...args,
+        form: form
     }
 });
 
--- core/ui/prime/components/src/lib/input-email/input-email.stories.ts
+++ core/ui/prime/components/src/lib/input-email/input-email.stories.ts
@@ -1,20 +1,37 @@
 import { Meta, moduleMetadata, Story } from "@storybook/angular";
 import { InputEmailComponent } from "./input-email.component";
 import { InputTextModule } from "primeng/inputtext";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+
+const form: FormGroup = new FormGroup({
+    email: new FormControl("", []),
+});
 
 export default {
     component: InputEmailComponent,
     title: "Components/InputEmail",
     decorators: [
         moduleMetadata({
-            imports: [ InputTextModule ]
+            imports: [ InputTextModule, ReactiveFormsModule ]
         })
     ]
 } as Meta
 
 const Template: Story = args => ({
+    template:  `<form [formGroup]="form">
+                <lens-input-email
+                    formControlName="email"
+                    name="email"
+                    [id]="id"
+                    [icon]="icon"
+                    [iconAlign]="iconAlign"
+                    [placeholder]="placeholder"
+                    [spinIcon]="spinIcon">
+                </lens-input-email>
+                </form>`,
     props: {
-        ...args
+        ...args,
+        form: form
     }
 });
 
