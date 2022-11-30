591b2d256f6cb4ee4acde0ab5bf1962e96d87884
Draghici Alexandru George <dageorgero@gmail.com>
2022-11-30 13:49:38 +0200

    commented line with dialogService popup in ErrorHandlerService from lens-framework

--- core/app/abstract-ui/src/lib/services/error-handler.service.ts
+++ core/app/abstract-ui/src/lib/services/error-handler.service.ts
@@ -1,25 +1,19 @@
-import { Injectable, OnDestroy } from '@angular/core';
-import { ErrorHandlerService as BaseErrorHandlerService } from '@lens/app-abstract';
-import { Subject, takeUntil } from 'rxjs';
-import { DialogService } from '.';
-import { ErrorDetailComponent } from '../components';
+import { Injectable, OnDestroy } from "@angular/core";
+import { ErrorHandlerService as BaseErrorHandlerService } from "@lens/app-abstract";
+import { Subject, takeUntil } from "rxjs";
+import { DialogService } from ".";
+import { ErrorDetailComponent } from "../components";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ErrorHandlerService implements OnDestroy {
-
   private readonly destroy$ = new Subject<void>();
-  constructor(
-    errorHandlerService: BaseErrorHandlerService,
-    dialogService: DialogService) {
-    errorHandlerService.OnError.pipe(
-      takeUntil(this.destroy$)
-    )
-      .subscribe(error => {
-        console.log(error);
-        dialogService.open(ErrorDetailComponent, { header: "An error occurred", data: { error: error } });
-      });
+  constructor(errorHandlerService: BaseErrorHandlerService, dialogService: DialogService) {
+    errorHandlerService.OnError.pipe(takeUntil(this.destroy$)).subscribe((error) => {
+      console.log(error);
+      //dialogService.open(ErrorDetailComponent, { header: "An error occurred", data: { error: error } });
+    });
   }
 
   ngOnDestroy(): void {
