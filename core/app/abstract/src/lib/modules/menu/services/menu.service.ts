64e7420b468428af4ceeeceb3d6026f9b347c9f9
Bart van der Vliet <bartv@od.org>
2022-12-07 12:02:16 +0100

    Fixed lintnig errors

--- core/app/abstract/src/lib/modules/menu/services/menu.service.ts
+++ core/app/abstract/src/lib/modules/menu/services/menu.service.ts
@@ -1,89 +1,84 @@
-import { Injectable, Inject, Optional } from '@angular/core';
-import { map, Observable, BehaviorSubject } from 'rxjs';
-import { APP_INFO, AppInfo } from '../../app-info';
-import { UserContextService } from '../../user-context';
-import { MenuItem } from '../models';
+import { Injectable, Inject, Optional } from "@angular/core";
+import { map, Observable, BehaviorSubject } from "rxjs";
+import { APP_INFO, AppInfo } from "../../app-info";
+import { UserContextService } from "../../user-context";
+import { MenuItem } from "../models";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root",
 })
 export class MenuService {
+	private allMenuItems: MenuItem[] = [];
+	private menuItems$ = new BehaviorSubject<MenuItem[]>(this.allMenuItems);
 
-  private allMenuItems: MenuItem[] = [];
-  private menuItems$ = new BehaviorSubject<MenuItem[]>(this.allMenuItems);
+	constructor(@Inject(APP_INFO) private appInfo: AppInfo, @Optional() private userContext: UserContextService) {
+		if (this.userContext) {
+			// Whenever something changes in the UserContext, refresh the menu by refiltering the menuItems.
+			//TODO: Make the filter work again
+			userContext.changed$.subscribe(() => this.menuItems$.next(this.allMenuItems.filter(this.filter, this)));
+			userContext.changed$.subscribe(() => this.menuItems$.next(this.allMenuItems));
+		}
+	}
 
-  constructor(@Inject(APP_INFO) private appInfo: AppInfo, @Optional() private userContext: UserContextService) {
-    if (this.userContext) {
-      // Whenever something changes in the UserContext, refresh the menu by refiltering the menuItems.
-      //TODO: Make the filter work again
-      userContext.changed$.subscribe(() => this.menuItems$.next(this.allMenuItems.filter(this.filter, this)));
-      userContext.changed$.subscribe(() => this.menuItems$.next(this.allMenuItems));
-    }
-  }
+	addMenuItems(menuItems: MenuItem | MenuItem[]): void {
+		if (Array.isArray(menuItems)) this.allMenuItems.push(...menuItems);
+		else this.allMenuItems.push(menuItems);
 
-  addMenuItems(menuItems: MenuItem | MenuItem[]): void {
-    if (Array.isArray(menuItems))
-      this.allMenuItems.push(...menuItems);
-    else
-      this.allMenuItems.push(menuItems);
+		//TODO: Make the filter work again
+		this.menuItems$.next(this.allMenuItems);
+		// this.menuItems$.next(this.allMenuItems.filter(this.filter, this));
+	}
 
-    //TODO: Make the filter work again
-    this.menuItems$.next(this.allMenuItems);
-    // this.menuItems$.next(this.allMenuItems.filter(this.filter, this));
-  }
+	getMenuItems(): Observable<MenuItem[]> {
+		return this.menuItems$;
+	}
 
-  getMenuItems(): Observable<MenuItem[]> {
-    return this.menuItems$;
-  }
+	public get isEmpty(): Observable<boolean> {
+		return this.getMenuItems().pipe(map((menuItems) => menuItems.length === 0));
+	}
 
-  public get isEmpty(): Observable<boolean> {
-    return this.getMenuItems().pipe(map(menuItems => menuItems.length == 0));
-  }
+	filter(navItem: MenuItem): MenuItem | null {
+		// if environment-filter is false, don't show
+		if (navItem.envfilter && navItem.envfilter?.indexOf(this.appInfo.environment ?? "") < 0) {
+			return null;
+		}
 
-  filter(navItem: MenuItem): MenuItem | null {
-    // if environment-filter is false, don't show
-    if (navItem.envfilter && navItem.envfilter?.indexOf(this.appInfo.environment ?? '') < 0) {
-      return null;
-    }
+		if (this.userContext) {
+			// if claim-filter is false, don't show
+			if (navItem.claimfilter && !navItem.claimfilter.some((claim) => this.userContext.HasClaim(claim))) {
+				return null;
+			}
 
-    if (this.userContext) {
-      // if claim-filter is false, don't show
-      if (navItem.claimfilter && !navItem.claimfilter.some(claim => this.userContext.HasClaim(claim))) {
-        return null;
-      }
+			// if role-filter is false, don't show
+			if (navItem.rolefilter && !navItem.rolefilter.some((role) => this.userContext.IsInRole(role))) {
+				return null;
+			}
 
-      // if role-filter is false, don't show
-      if (navItem.rolefilter && !navItem.rolefilter.some(role => this.userContext.IsInRole(role))) {
-        return null;
-      }
+			// if no filter and not authenticated, don't show
+			if (((navItem.claimfilter?.length ?? 0) === 0 || (navItem.rolefilter?.length ?? 0) === 0) && !this.userContext.IsAuthenticated) {
+				return null;
+			}
 
-      // if no filter and not authenticated, don't show
-      if (((navItem.claimfilter?.length ?? 0) === 0 || (navItem.rolefilter?.length ?? 0) === 0) && !this.userContext.IsAuthenticated) {
-        return null;
-      }
+			// if 'only show when NOT authenticated' but is authenticated, don't show
+			if (navItem.anonymousonly && this.userContext.IsAuthenticated) {
+				return null;
+			}
+		} else {
+			if (navItem.claimfilter?.length ?? 0 >= 1) {
+				return null;
+			}
 
-      // if 'only show when NOT authenticated' but is authenticated, don't show
-      if (navItem.anonymousonly && this.userContext.IsAuthenticated) {
-        return null;
-      }
-    }
-    else 
-    {
-      if (navItem.claimfilter?.length ?? 0 >= 1) {
-        return null;
-      }
+			if (navItem.rolefilter?.length ?? 0 >= 1) {
+				return null;
+			}
+		}
 
-      if (navItem.rolefilter?.length ?? 0 >= 1) {
-        return null;
-      }
-    }
+		// filter possible sub-menu items
+		if (navItem.items) {
+			navItem.items = navItem.items.filter(this.filter, this);
+		}
 
-    // filter possible sub-menu items
-    if (navItem.items) {
-      navItem.items = navItem.items.filter(this.filter, this);
-    }
-
-    // show the menu-item
-    return navItem;
-  }
+		// show the menu-item
+		return navItem;
+	}
 }
