7383bdd44ecff2cceab7e65c1557d9ebb2e69f7b
Bart van der Vliet <bartv@od.org>
2022-12-07 09:42:27 +0100

    Fixed dialog buttons to the bottom of the dialog.

--- core/ui/prime/components/src/lib/dialog/dialog.component.ts
+++ core/ui/prime/components/src/lib/dialog/dialog.component.ts
@@ -3,29 +3,32 @@ import { DynamicDialogConfig, DynamicDialogRef } from "primeng/dynamicdialog";
 import { DialogComponent as AbstractDialogComponent, DialogConfig, DialogRef } from "@lens/app-abstract-ui";
 
 @Component({
-    templateUrl: "dialog.component.html",
-    styleUrls: [ "dialog.component.scss" ]
+	templateUrl: "dialog.component.html",
+	styleUrls: ["dialog.component.scss"],
 })
 export class DialogComponent implements OnInit {
-    public component!: AbstractDialogComponent;
+	public component!: AbstractDialogComponent;
 
-    constructor(
-        private readonly ref: DynamicDialogRef,
-        public readonly config: DynamicDialogConfig,
-        private readonly viewContainerRef: ViewContainerRef,
-        private readonly environmentInjector: EnvironmentInjector
-    ) { }
+	constructor(
+		private readonly ref: DynamicDialogRef,
+		public readonly config: DynamicDialogConfig,
+		private readonly viewContainerRef: ViewContainerRef,
+		private readonly environmentInjector: EnvironmentInjector
+	) {}
 
-    public ngOnInit(): void {
-        const newEnvironmentInjector = createEnvironmentInjector([
-            { provide: "LensDialogRef", useValue: <DialogRef>this.ref},
-            { provide: "LensDialogConfig", useValue: <DialogConfig>this.config},
-        ], this.environmentInjector);
-        const componentRef = this.viewContainerRef.createComponent(this.config.data.componentType, { environmentInjector: newEnvironmentInjector });
-        this.component = componentRef.instance as AbstractDialogComponent
-    }
+	public ngOnInit(): void {
+		const newEnvironmentInjector = createEnvironmentInjector(
+			[
+				{ provide: "LensDialogRef", useValue: <DialogRef>this.ref },
+				{ provide: "LensDialogConfig", useValue: <DialogConfig>this.config },
+			],
+			this.environmentInjector
+		);
+		const componentRef = this.viewContainerRef.createComponent(this.config.data.componentType, { environmentInjector: newEnvironmentInjector });
+		this.component = componentRef.instance as AbstractDialogComponent;
+	}
 
-    public onCloseClick() {
-        this.ref.close();
-    }
+	public onCloseClick() {
+		this.ref.close();
+	}
 }
