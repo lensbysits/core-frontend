0e703276126ea0e3b94f2b270270c34f3d3e884e
Johan Hoek <johan.hoek@fresh-minds.nl>
2022-12-20 16:44:52 +0100

    Added id's and classes to find elements in the cypress tests + fixed transpile errors

--- core/security/oauth/src/lib/oauthentication.module.ts
+++ core/security/oauth/src/lib/oauthentication.module.ts
@@ -1,76 +1,63 @@
-import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {
-  AuthModule,
-  AutoLoginAllRoutesGuard,
-  LogLevel,
-  OpenIdConfiguration,
-  StsConfigLoader,
-  StsConfigStaticLoader,
-} from 'angular-auth-oidc-client';
-import { AppConfigurationService, UserContextService } from '@lens/app-abstract';
-import { AuthenticationService, AuthGuard } from '@lens/security-abstract';
-import { AuthenticationRedirectComponent } from './components';
-import { 
-  OAuthenticationService, 
-  UserContextService as oAuthUserContextService 
-} from './services';
+import { InjectionToken, ModuleWithProviders, NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AuthModule, AutoLoginAllRoutesGuard, LogLevel, OpenIdConfiguration, StsConfigLoader, StsConfigStaticLoader } from "angular-auth-oidc-client";
+import { AppConfigurationService, UserContextService } from "@lens/app-abstract";
+import { AuthenticationService, AuthGuard } from "@lens/security-abstract";
+import { AuthenticationRedirectComponent } from "./components";
+import { OAuthenticationService, UserContextService as oAuthUserContextService } from "./services";
 
 const APP_OAUTH_CONFIGURATION = new InjectionToken("APP_OAUTH_CONFIGURATION");
 
 const stsConfigLoaderFactory = (configuration: OpenIdConfiguration) => {
-  if (!configuration) {
-    throw new Error('make sure to pass in an auth-config');
-  }
+	if (!configuration) {
+		throw new Error("make sure to pass in an auth-config");
+	}
 
-  return new StsConfigStaticLoader(configuration);
+	return new StsConfigStaticLoader(configuration);
 };
 
 function appOAuthConfigurationFactory(appConfigurationService: AppConfigurationService): OpenIdConfiguration {
-  return {
-    authority: appConfigurationService.getSettings("identity.client.auth.authority"),
-    redirectUrl: appConfigurationService.getSettings("identity.client.auth.redirectUri"),
-    postLogoutRedirectUri: appConfigurationService.getSettings("identity.client.auth.postLogoutRedirectUri"),
-    clientId: appConfigurationService.getSettings("identity.client.auth.clientId"),
-    scope: appConfigurationService.getSettings("identity.guard.scopes").scopes.join(' '),
-    responseType: 'code',
-    silentRenew: true,
-    useRefreshToken: true,
-    logLevel: LogLevel.Debug
-  };
+	return {
+		authority: appConfigurationService.getSettings<string>("identity.client.auth.authority"),
+		redirectUrl: appConfigurationService.getSettings<string>("identity.client.auth.redirectUri"),
+		postLogoutRedirectUri: appConfigurationService.getSettings<string>("identity.client.auth.postLogoutRedirectUri"),
+		clientId: appConfigurationService.getSettings<string>("identity.client.auth.clientId"),
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		scope: appConfigurationService.getSettings<any>("identity.guard.scopes").scopes.join(" "),
+		responseType: "code",
+		silentRenew: true,
+		useRefreshToken: true,
+		logLevel: LogLevel.Debug
+	};
 }
 
 @NgModule({
-  declarations: [
-    AuthenticationRedirectComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthModule.forRoot({
-      loader: {
-        provide: StsConfigLoader,
-        useFactory: stsConfigLoaderFactory,
-        deps: [ APP_OAUTH_CONFIGURATION ]
-      },
-    }),
-  ],
-  exports: [AuthModule],
+	declarations: [AuthenticationRedirectComponent],
+	imports: [
+		CommonModule,
+		AuthModule.forRoot({
+			loader: {
+				provide: StsConfigLoader,
+				useFactory: stsConfigLoaderFactory,
+				deps: [APP_OAUTH_CONFIGURATION]
+			}
+		})
+	],
+	exports: [AuthModule]
 })
 export class OAuthenticationModule {
-  static bootstrap = [
-    AuthenticationRedirectComponent
-  ];
+	static bootstrap = [AuthenticationRedirectComponent];
 
-  static forRoot(): ModuleWithProviders<OAuthenticationModule> {
-    return {
-      ngModule: OAuthenticationModule,
-      providers: [
-        { provide: APP_OAUTH_CONFIGURATION, useFactory: appOAuthConfigurationFactory, deps: [ AppConfigurationService ] },
-        { provide: AuthenticationService, useExisting: OAuthenticationService },
-        { provide: UserContextService, useClass: oAuthUserContextService },
-        { provide: AuthGuard, useClass: AutoLoginAllRoutesGuard },
-        AutoLoginAllRoutesGuard
-      ],
-    };
-  }
+	static forRoot(): ModuleWithProviders<OAuthenticationModule> {
+		return {
+			ngModule: OAuthenticationModule,
+			providers: [
+				{ provide: APP_OAUTH_CONFIGURATION, useFactory: appOAuthConfigurationFactory, deps: [AppConfigurationService] },
+				{ provide: AuthenticationService, useExisting: OAuthenticationService },
+				{ provide: UserContextService, useClass: oAuthUserContextService },
+				{ provide: AuthGuard, useClass: AutoLoginAllRoutesGuard },
+				AutoLoginAllRoutesGuard
+			]
+		};
+	}
 }
