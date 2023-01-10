d6873d5880002daedb2e8baf5c1a314ed7324d67
Bart van der Vliet <bartv@od.org>
2023-01-10 15:46:28 +0100

    Fixed unneccesary providers.

--- core/app/abstract-ui/src/lib/components/app/app.component.ts
+++ core/app/abstract-ui/src/lib/components/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AppConfigurationService } from '@lens/app-abstract';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'frontend-root',
-  template: '<router-outlet></router-outlet>',
-  providers: [TranslateService]
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent implements OnInit {
     constructor(
