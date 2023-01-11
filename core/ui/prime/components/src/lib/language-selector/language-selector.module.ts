04d2647da049cbcd4a18aa43fb6ecc7c1b607414
Johan Hoek <johan.hoek@fresh-minds.nl>
2023-01-11 17:22:50 +0100

    Updated language initialization to support mulilingual menu and language picker

--- core/ui/prime/components/src/lib/language-selector/language-selector.module.ts
+++ core/ui/prime/components/src/lib/language-selector/language-selector.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { LanguageSelectorComponent } from "./language-selector.component";
 import { DropdownModule as PrimeDropdownModule } from "primeng/dropdown";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
     imports: [
+        CommonModule,
         FormsModule,
         PrimeDropdownModule
     ],
