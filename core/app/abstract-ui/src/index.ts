6b098f18cb05193e321dd4271288950aae7a5199
johanh-od <johanh@od.org>
2023-01-23 11:47:32 +0100

    Added validator for key value pair dropdowns

--- core/app/abstract-ui/src/index.ts
+++ core/app/abstract-ui/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/app-abstract-ui.module';
 export * from './lib/components';
 export * from './lib/directives';
 export * from './lib/services';
+export * from './lib/validators';
