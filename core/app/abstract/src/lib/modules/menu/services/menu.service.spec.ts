aff90a985fbb3592462e483149dca07388b4d71b
Bart van der Vliet <bartv@od.org>
2023-01-25 08:31:23 +0100

    Fixed tests

--- core/app/abstract/src/lib/modules/menu/services/menu.service.spec.ts
+++ core/app/abstract/src/lib/modules/menu/services/menu.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { AppInfo, APP_INFO } from '../../app-info';
 
 import { MenuService } from './menu.service';
 
@@ -6,7 +7,11 @@ describe('MenuService', () => {
   let service: MenuService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+		providers: [
+			{ provide: APP_INFO, useValue: new AppInfo() }
+		]
+	});
     service = TestBed.inject(MenuService);
   });
 
