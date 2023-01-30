3a10e6322f6fed210218bdb790c57472fb6e0516
johanh-od <johanh@od.org>
2023-01-30 14:47:41 +0100

    wip

--- core/ui/prime/components/src/lib/tree-filter/tree.component.ts
+++ core/ui/prime/components/src/lib/tree-filter/tree.component.ts
@@ -4,35 +4,40 @@ import { TreeNode } from "primeng/api";
 import { InputBaseComponent } from "../input-base/input-base.component";
 
 @Component({
-    selector: "lens-tree", 
-    templateUrl: "tree.component.html",
-    providers: [
-        { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => TreeComponent), multi: true }
-    ]
+	selector: "lens-tree",
+	templateUrl: "tree.component.html",
+	providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => TreeComponent), multi: true }]
 })
 export class TreeComponent extends InputBaseComponent implements OnInit {
 	@Input()
-	public nodes!: TreeNode<unknown>[]
+	public nodes!: TreeNode<unknown>[];
 	@Input()
-	public filter = false
+	public filter = false;
 	@Input()
-	public filterMode = "strict"
+	public filterMode = "strict";
 	@Input()
-	public selectionMode = "single"
+	public selectionMode = "single";
 	@Input()
-	public selectedKeys?: string[]|number[]
+	public selectedKeys?: string[] | number[];
 
 	@Output()
-	public nodeSelected = new EventEmitter<TreeNode<unknown>>()
-	public nodeUnselected = new EventEmitter<TreeNode<unknown>>()
+	public nodeSelected = new EventEmitter<TreeNode<unknown>>();
+	public nodeUnselected = new EventEmitter<TreeNode<unknown>>();
 
 	ngOnInit(): void {
-		for(const node of this.nodes){
-			node.selectable = !this.isDisabled
+		for (const node of this.nodes) {
+			// if (!node.expandedIcon) {
+			// 	node.expandedIcon = "pi pi-folder-open";
+			// }
+
+			// if (!node.collapsedIcon) {
+			// 	node.collapsedIcon = "pi pi-folder";
+			// }
+			node.selectable = !this.isDisabled;
 		}
 	}
 
-	public selectedNodes():TreeNode<unknown>[]{
+	public selectedNodes(): TreeNode<unknown>[] {
 		return [];
 	}
 }
