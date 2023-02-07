b88557491b577bd0a82586aa5e4eb72c4a06061f
Draghici Alexandru George <dageorgero@gmail.com>
2023-02-07 21:16:38 +0200

    clean main component

--- core/ui/bootstrap/layout/src/lib/components/main/main.component.ts
+++ core/ui/bootstrap/layout/src/lib/components/main/main.component.ts
@@ -1,10 +1,9 @@
 import { Component, Renderer2 } from "@angular/core";
-//import { MenuService } from "../menu/menu.service";
-import { PrimeNGConfig } from "primeng/api";
 import {
 	ILayoutConfiguration,
 	LayoutConfigurationService
 } from "@lens/app-abstract";
+//import { MenuService } from "../menu/menu.service";
 
 @Component({
 	selector: "ui-main",
@@ -12,26 +11,12 @@ import {
 	styleUrls: ["./main.component.scss"]
 })
 export class AppMainComponent {
-	rotateMenuButton = false;
-	topbarMenuActive = false;
-	overlayMenuActive = false;
-	staticMenuDesktopInactive = false;
-	staticMenuMobileActive = false;
-	layoutMenuScroller: HTMLDivElement | undefined = undefined;
-	menuClick = false;
-	topbarItemClick = false;
-	activeTopbarItem: any;
-	menuHoverActive = false;
-	configActive = false;
-	configClick = false;
-
 	layoutConfiguration: ILayoutConfiguration = {};
 
 	constructor(
 		public readonly renderer: Renderer2,
 		//private readonly menuService: MenuService,
-		readonly layoutConfigurationService: LayoutConfigurationService,
-		private primengConfig: PrimeNGConfig
+		readonly layoutConfigurationService: LayoutConfigurationService
 	) {
 		layoutConfigurationService.layoutConfiguration$.subscribe(
 			(config) => (this.layoutConfiguration = config)
