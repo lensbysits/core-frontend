4899cd8007c938281c855c359ad337268d30a66a
jstorteboom <johan@storteboom.net>
2023-03-27 16:37:24 +0200

    Get app brand name from appInfo.name in configuration.json.

--- core/ui/matero/layout/src/lib/theme/widgets/branding.component.ts
+++ core/ui/matero/layout/src/lib/theme/widgets/branding.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AppConfigurationService } from '@lens/app-abstract';
 
 @Component({
   selector: 'app-branding',
   template: `
     <a class="d-inline-block text-nowrap r-full text-reset" href="/">
       <img src="./assets/images/matero.png" class="brand-logo align-middle m-2" alt="logo" />
-      <span class="align-middle f-s-16 f-w-500 m-x-8">MATERO</span>
+      <span class="align-middle f-s-16 f-w-500 m-x-8">{{appName}}</span>
     </a>
   `,
   styles: [
@@ -17,4 +18,9 @@ import { Component } from '@angular/core';
     `,
   ],
 })
-export class BrandingComponent {}
+export class BrandingComponent {
+  appName = 'set appInfo.name';
+  constructor(appConfigurationService: AppConfigurationService) {
+    this.appName = appConfigurationService.getSettings("appInfo.name")
+  }
+}
