92f3c861009955ac2b7f3031a6cbf621c3de875e
johanh-od <johanh@od.org>
2023-03-27 15:57:18 +0200

    added rtl support and fixed language refresh bug

--- core/ui/prime/components/src/lib/language-selector/language-selector.component.ts
+++ core/ui/prime/components/src/lib/language-selector/language-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Renderer2 } from "@angular/core";
 import { LanguageService } from "@lens/app-abstract";
 import { TranslateService } from "@ngx-translate/core";
 
@@ -10,10 +10,9 @@ export class LanguageSelectorComponent implements OnInit {
 	public languages!: ILanguage[];
 	public selectedLanguage!: ILanguage;
 	public translationsLoaded = false;
+	private rtlLanguages = ["ar", "he", "fa", "ur"];
 
-	constructor(
-		private translateService: TranslateService, 
-		private languageService: LanguageService) {}
+	constructor(private translateService: TranslateService, private languageService: LanguageService, private renderer: Renderer2) {}
 
 	public ngOnInit(): void {
 		this.languageService.onTranslationsLoaded(() => {
@@ -33,7 +32,30 @@ export class LanguageSelectorComponent implements OnInit {
 		if (this.translateService.getLangs().indexOf(lang.value) !== -1) {
 			this.translateService.use(lang.value).subscribe(() => {
 				this.createAvailableLanguageList();
+				this.setReadDirection(lang.value);
 			});
+		}
+	}
+
+	private setReadDirection(lang: any) {
+		if(!this.languageService.rtlStylesheetLocation && this.rtlLanguages.includes(lang)){
+			console.error("No RTL stylesheet location is defined in the app configuration");	
+			return;
+		}
+
+		if (this.rtlLanguages.includes(lang)) {
+			const link = this.renderer.createElement("link");
+			link.rel = "stylesheet";
+			link.href = this.languageService.rtlStylesheetLocation;
+			this.renderer.appendChild(document.head, link);
+		} else {
+			const links = document.getElementsByTagName("link");
+			for (let i = 0; i < links.length; i++) {
+				const link = links[i];
+				if (link.href.includes(this.languageService.rtlStylesheetLocation ?? '')) {
+					this.renderer.removeChild(document.head, link);
+				}
+			}
 		}
 	}
 
