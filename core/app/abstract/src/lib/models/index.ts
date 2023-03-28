f274f9a5d312a829c02a178b421d4bb1f88a0113
jstorteboom <johan@storteboom.net>
2023-03-28 12:32:21 +0200

    Move query.model and result.model to abstract lib.

--- core/app/abstract/src/lib/models/index.ts
+++ core/app/abstract/src/lib/models/index.ts
@@ -1,3 +1,7 @@
 export * from "./error-message.interface";
 export * from "./key-value-pair.class";
+export * from "./query.model.interface";
+export * from "./query.model";
+export * from "./result.model";
+export * from "./result-list.model";
 
--- core/app/abstract/src/lib/models/result-list.model.ts
+++ core/app/abstract/src/lib/models/result-list.model.ts
@@ -1,6 +1,6 @@
-export class Result<T> {
-	valueType?: string;
-	value?: T | undefined;
+import { Result } from "./result.model";
+
+export class ResultList<T> extends Result<T[]> {
 	valueSize?: number | undefined;
 	totalSize?: number | undefined;
 	pageSize?: number | undefined;
