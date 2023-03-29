010c4052f73b424c60ab2ae7b9662c553d8e21e8
Jonathan Overeem <jonathan.overeem@matrix.nl>
2023-03-29 16:20:18 +0200

    Able to set layout settings for ng-matero  layout

--- core/ui/matero/layout/src/lib/theme/widgets/branding.component.ts
+++ core/ui/matero/layout/src/lib/theme/widgets/branding.component.ts
@@ -5,7 +5,7 @@ import { AppConfigurationService } from '@lens/app-abstract';
   selector: 'app-branding',
   template: `
     <a class="d-inline-block text-nowrap r-full text-reset" href="/">
-      <img src="./assets/images/matero.png" class="brand-logo align-middle m-2" alt="logo" />
+      <img *ngIf="appLogo" src="{{appLogo}}" class="brand-logo align-middle m-2" alt="logo" />
       <span class="align-middle f-s-16 f-w-500 m-x-8">{{appName}}</span>
     </a>
   `,
@@ -13,14 +13,18 @@ import { AppConfigurationService } from '@lens/app-abstract';
     `
       .brand-logo {
         width: 30px;
-        height: 30px;
+        max-height: 30px;
       }
     `,
   ],
 })
 export class BrandingComponent {
   appName = 'set appInfo.name';
+  appLogo: string;
+
+
   constructor(appConfigurationService: AppConfigurationService) {
     this.appName = appConfigurationService.getSettings("appInfo.name")
+    this.appLogo = appConfigurationService.getSettings("appInfo.logo")
   }
 }
