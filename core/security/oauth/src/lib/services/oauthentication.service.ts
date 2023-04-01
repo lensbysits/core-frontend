8a84c634bf1391be319e1b344f9377781885eca7
Draghici Alexandru George <dageorgero@gmail.com>
2023-04-01 16:44:55 +0300

    fix: UserData model on UserContextService, in core/security/oauth

--- core/security/oauth/src/lib/services/oauthentication.service.ts
+++ core/security/oauth/src/lib/services/oauthentication.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { UserData } from "@lens/app-abstract";
 import { AuthenticationService } from "@lens/security-abstract";
 import { OidcSecurityService } from "angular-auth-oidc-client";
 import { map, Observable } from "rxjs";
@@ -8,11 +9,20 @@ import { map, Observable } from "rxjs";
 })
 export class OAuthenticationService implements AuthenticationService {
 	isAuthenticated$!: Observable<boolean>;
-	userData$: Observable<unknown> = new Observable<unknown>();
+	userData$: Observable<UserData> = new Observable<UserData>();
 
 	constructor(private oidcSecurityService: OidcSecurityService) {
-		this.isAuthenticated$ = oidcSecurityService.isAuthenticated$.pipe(map((authenticatedResult) => authenticatedResult.isAuthenticated));
-		this.userData$ = oidcSecurityService.userData$.pipe(map((userDataResult) => userDataResult.userData));
+		this.isAuthenticated$ = oidcSecurityService.isAuthenticated$.pipe(
+			map((authenticatedResult) => authenticatedResult.isAuthenticated)
+		);
+		this.userData$ = oidcSecurityService.userData$.pipe(
+			map((userDataResult) => userDataResult.userData)
+		);
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	getAccessToken(resource: string): Promise<string> {
+		throw new Error("Method not implemented.");
 	}
 
 	login(): void {
