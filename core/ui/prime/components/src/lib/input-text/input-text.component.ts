67d9934e6268faed5f532f91a6818eee3b1f39cc
dageorgero <82839687+dageorgero@users.noreply.github.com>
2023-07-19 01:33:58 +0300

    input text: added focusIn, focusOut events (#72)

--- core/ui/prime/components/src/lib/input-text/input-text.component.ts
+++ core/ui/prime/components/src/lib/input-text/input-text.component.ts
@@ -1,18 +1,31 @@
-import { Component, forwardRef, Input } from "@angular/core";
+import { Component, EventEmitter, forwardRef, Input, Output } from "@angular/core";
 import { NG_VALUE_ACCESSOR } from "@angular/forms";
 import { InputBaseComponent } from "../input-base/input-base.component";
 
 @Component({
-    selector: "lens-input-text", 
-    templateUrl: "input-text.component.html",
-    providers: [
-        { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => InputTextComponent), multi: true }
-    ]
+	selector: "lens-input-text",
+	templateUrl: "input-text.component.html",
+	providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => InputTextComponent), multi: true }]
 })
 export class InputTextComponent extends InputBaseComponent {
-    @Input() id!: string;
-    @Input() placeholder?: string;
-    @Input() icon!: string;
-    @Input() spinIcon = false;
-    @Input() iconAlign: "right" | "left" = "left";
+	@Input() public id!: string;
+	@Input() public placeholder?: string;
+	@Input() public icon!: string;
+	@Input() public spinIcon = false;
+	@Input() public iconAlign: "right" | "left" = "left";
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	@Output() public focusIn: EventEmitter<any> = new EventEmitter();
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	@Output() public focusOut: EventEmitter<any> = new EventEmitter();
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	public onFocusIn(event: any) {
+		this.focusIn.emit(event.target.value);
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	public onFocusOut(event: any) {
+		this.focusOut.emit(event.target.value);
+	}
 }
