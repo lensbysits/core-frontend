ccde16c62a889c89802007db299feb0a35a155f4
Draghici Alexandru George <dageorgero@gmail.com>
2023-08-18 14:11:14 +0300

    add userid as userData on userContext

--- core/security/oauth/src/lib/services/user-context.service.ts
+++ core/security/oauth/src/lib/services/user-context.service.ts
@@ -5,36 +5,25 @@ import { takeUntil } from "rxjs";
 
 @Injectable()
 export class UserContextService extends DefaultUserContextService {
+	constructor(authenticationService: AuthenticationService, @Optional() additionalClaimsService: AdditionalClaimsService) {
+		super(additionalClaimsService);
+		authenticationService.userData$.pipe(takeUntil(this.destroy$)).subscribe(userData => {
+			const uData: UserData = {
+				Username: userData?.name,
+				Roles: userData?.role,
+				Claims: [
+					{ name: "app", value: userData?.app },
+					{ name: "personid", value: userData?.personid },
+					{ name: "sub", value: userData?.sub },
+					{ name: "preferred_username", value: userData?.preferred_username },
+					{ name: "email_verified", value: userData?.email_verified }
+				]
+			};
+			super.Set(uData, super.IsAuthenticated);
+		});
 
-
-  constructor(
-	authenticationService: AuthenticationService,
-	@Optional() additionalClaimsService: AdditionalClaimsService
-  ) {
-    super(additionalClaimsService);
-    authenticationService.userData$.pipe(
-      takeUntil(this.destroy$)
-    )
-      .subscribe((userData) => {
-        const uData: UserData = {
-          Username: userData?.name,
-          Roles: userData?.role,
-          Claims: [
-            { name: 'app', value: userData?.app },
-            { name: 'personid', value: userData?.personid },
-            { name: 'sub', value: userData?.sub },
-            { name: 'preferred_username', value: userData?.preferred_username },
-            { name: 'email_verified', value: userData?.email_verified },
-          ]
-        }
-        super.Set(uData, super.IsAuthenticated);
-      });
-
-    authenticationService.isAuthenticated$.pipe(
-      takeUntil(this.destroy$)
-    )
-      .subscribe((isAuthenticated) => {
-        super.Set({}, isAuthenticated);
-      });
-  }
+		authenticationService.isAuthenticated$.pipe(takeUntil(this.destroy$)).subscribe(isAuthenticated => {
+			super.Set({}, isAuthenticated);
+		});
+	}
 }
