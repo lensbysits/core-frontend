b2c9d81a3b58bd0b9c6b2430758d4f742eee1e42
Draghici Alexandru George <dageorgero@gmail.com>
2023-09-13 23:04:24 +0300

    add tooltip option to 'lens-button'

--- core/ui/prime/components/src/lib/button/button.component.ts
+++ core/ui/prime/components/src/lib/button/button.component.ts
@@ -1,32 +1,31 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
-  selector: 'lens-button',
-  templateUrl: './button.component.html'
+	selector: "lens-button",
+	templateUrl: "./button.component.html"
 })
 export class ButtonComponent {
-  @Input() public label = "";
-  @Input() public type: "primary" | "secondary" | "success" | "info" | "warning" | "danger" = "primary";
-  @Input() public appearance: "default" | "raised" | "raised-text" | "rounded" | "rounded-outlined" | "rounded-text" | "text" | "raised-text" = "default";
-  @Input() public size: "small" | "default" | "large" = "default";
-  @Output() public clicked = new EventEmitter<MouseEvent>();
-  @Input() public icon = "";
-  @Input() public disabled = false;
+	@Input() public label = "";
+	@Input() public type: "primary" | "secondary" | "success" | "info" | "warning" | "danger" = "primary";
+	@Input() public appearance: "default" | "raised" | "raised-text" | "rounded" | "rounded-outlined" | "rounded-text" | "text" | "raised-text" =
+		"default";
+	@Input() public size: "small" | "default" | "large" = "default";
+	@Input() public icon = "";
+	@Input() public disabled = false;
+	@Input() public tooltip!: string;
+	@Input() public tooltipPosition: "left" | "top" | "bottom" | "right" = "left";
 
-  public isRaised(): boolean {
-    return this.appearance === "raised"
-           || this.appearance === "raised-text";
-  }
+	@Output() public clicked = new EventEmitter<MouseEvent>();
 
-  public isRounded(): boolean {
-    return this.appearance === "rounded"
-           || this.appearance === "rounded-outlined"
-           || this.appearance === "rounded-text";
-  }
+	public isRaised(): boolean {
+		return this.appearance === "raised" || this.appearance === "raised-text";
+	}
 
-  public isText(): boolean {
-    return this.appearance === "text"
-           || this.appearance === "raised-text"
-           || this.appearance === "rounded-text";
-  }
+	public isRounded(): boolean {
+		return this.appearance === "rounded" || this.appearance === "rounded-outlined" || this.appearance === "rounded-text";
+	}
+
+	public isText(): boolean {
+		return this.appearance === "text" || this.appearance === "raised-text" || this.appearance === "rounded-text";
+	}
 }
