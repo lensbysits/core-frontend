0caca42314649bda6e1bc7d03143df77e6fef6ca
Draghici Alexandru George <dageorgero@gmail.com>
2023-09-14 18:57:44 +0300

    lens-card update: refactored html template, added lens-card-buttons as a new content section beside card-title

--- core/ui/prime/components/src/lib/card/card.component.ts
+++ core/ui/prime/components/src/lib/card/card.component.ts
@@ -2,10 +2,12 @@ import { Component, Input } from "@angular/core";
 
 @Component({
 	selector: "lens-card",
-	templateUrl: "card.component.html"
+	templateUrl: "card.component.html",
+	styleUrls: ["./card.component.scss"]
 })
 export class CardComponent {
 	@Input() public header!: string;
-	@Input() public subheader!: string;
+	@Input() public title!: string;
+	@Input() public subtitle!: string;
 	@Input() public styleClass!: string;
 }
