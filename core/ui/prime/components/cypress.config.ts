4fe89048dc7475622b4f6559232fcaf3ba0f6c5e
Draghici Alexandru George <dageorgero@gmail.com>
2023-11-09 18:52:19 +0200

    nx update to latest version

--- core/ui/prime/components/cypress.config.ts
+++ core/ui/prime/components/cypress.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'cypress';
-import { nxComponentTestingPreset } from '@nrwl/angular/plugins/component-testing';
+import { defineConfig } from "cypress";
+import { nxComponentTestingPreset } from "@nx/angular/plugins/component-testing";
 
 export default defineConfig({
-  component: nxComponentTestingPreset(__filename),
+	component: nxComponentTestingPreset(__filename)
 });
